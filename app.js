b2a2d24cabc7118aa6856ee2c5a05c042a367418
3lv27 <elvin.gm27@gmail.com>
2017-12-08 14:14:14 +0100

    installing dependencies

--- app.js
+++ app.js
@@ -1,16 +1,52 @@
-let express = require('express');
-let path = require('path');
-let favicon = require('serve-favicon');
-let logger = require('morgan');
-let cookieParser = require('cookie-parser');
-let bodyParser = require('body-parser');
+'use strict';
 
-let index = require('./routes/index');
-let users = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const MongoStore = require('connect-mongo')(session);
 
-let app = express();
+const configurePassport = require('./helpers/passport');
+const response = require('../helpers/response');
 
-// uncomment after placing your favicon in /public
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+const app = express();
+
+// database config
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/democrapp', {
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE,
+  useMongoClient: true
+});
+
+// session config
+app.use(session({
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  }),
+  secret: 'some-string',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000
+  }
+}));
+
+// passpport config
+configurePassport();
+app.use(passport.initialize());
+app.use(passport.session());
+
+// middlewares
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -22,21 +58,19 @@ app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function(req, res) {
+  return response.notFound();
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // only send response if the error ocurred before sending the response
+  if (!res.headersSent) {
+    return response.unexpectedError();
+  };
 });
 
 module.exports = app;
